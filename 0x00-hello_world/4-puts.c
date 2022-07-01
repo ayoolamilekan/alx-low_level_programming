7cd72d259fab5b8d0025d4651ea89b8f538e7e8a
ayoolamilekan <lawaltemmanuel@gmail.com>
2022-06-30 19:09:22 -0700

    Yeah

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/*
+ * main - entry point
+ *
+ * char - declare char array
+ * initialize the array
+ *
+ * display string
+ * return always 0 (success)
+ */
+int main(void)
+{
+	char val[100];
+	strcpy(val, "Programming is like building a multilingual puzzle\n");
+
+	puts(val);
+	return (0);
+}
+
