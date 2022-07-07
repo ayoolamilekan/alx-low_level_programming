0008dfb2509c479466407da6fb251f519f44f4c3
ayoolamilekan <lawaltemmanuel@gmail.com>
2022-07-06 19:20:13 -0700

    Yeah

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * 
+ * Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	_putchar('c');
+	_putchar('o');
+	_putchar('p');
+	_putchar('\n');
+	return (0);
+}
+
