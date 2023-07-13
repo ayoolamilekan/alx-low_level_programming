7c47a89933fb60c1a9b5c1c4aee815bd6bd3b15f
polymathLTE <lawaltemmanuel@gmail.com>
2023-07-13 05:56:18 +0100

    Added task files
    README.md
    0-create_array.c
    main.h

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_array - function that creates an array of chars
+ * @size: size of char array to be created
+ * @c: the array will be initlzd with this char
+ *
+ * Description: function that creates an array of chars,
+ * and initializes it with a specific char
+ * Prototype: char *create_array(unsigned int size, char c);
+ * Returns NULL if size = 0
+ * Returns a pointer to the array, or NULL if it fails
+ *
+ * Return: success (char*) fail (NULL)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	/*define variables*/
+	char *cr;
+	size_t i;
+
+	cr = malloc(sizeof(int) * (size + 1));
+	if (size == 0 || cr == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < size)
+	{
+		cr[i] = c;
+		i++;
+	}
+	cr[i] = '\0';
+	return (cr);
+}
