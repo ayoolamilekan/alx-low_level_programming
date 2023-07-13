eabc7f639aa028fdd812d0e09fa7d16e9d154c16
polymathLTE <lawaltemmanuel@gmail.com>
2023-07-13 08:16:14 +0100

    Modified malloc to sizeof char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	char *cr;
 	size_t i;
 
-	cr = malloc(sizeof(int) * (size));
+	cr = malloc(sizeof(char) * (size));
 	if (size == 0 || cr == NULL)
 		return (NULL);
 
